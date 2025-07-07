14c08ba897db1c10a8d72fa3c97ec19edabb5c93
Pedro Façanha <pedrofacpeix@gmail.com>
2025-07-07 20:12:09 -0300

    Adicionando schema de mensagem

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/hello-world', response_class=HTMLResponse)
+def hello_world():
+    data = """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
+    return data
